a57caec72f1852d086107666e6a584c81d140cc9
Sumin Byeon <suminb@gmail.com>
2018-03-11 18:12:45 +0900

    My first Rust code

---
+++ README.md
@@ -0,0 +1,4 @@
+Rusty Exercise
+==============
+
+This is a collection of small projects to learn and to practice Rust.
---
+++ number-guess-game/src/main.rs
@@ -0,0 +1,36 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("A secret number has been chosen, but I won't tell you what it is.");
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(v) => v,
+            Err(_) => {
+                println!("Invalid input. Try again.");
+                continue;
+            }
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less    => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal   => {
+                println!("You got it!");
+                break;
+            }
+        };
+    }
+}
